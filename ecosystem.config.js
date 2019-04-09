e575c33bdf3540fd049be013217ef6e83dae930c
pavel.pavlenko <pavel.pavlenko@it-atlantic.com>
2019-04-09 11:57:43 +0600

    new sever

---
+++ ecosystem.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  apps : [{
+    name      : 'Video-CMS-back',
+    script    : 'server.js',
+    env: {
+      NODE_ENV: 'development',
+      NODE_PORT: 8769
+    },
+    env_production : {
+      NODE_ENV: 'production',
+      NODE_PORT: 8769
+    }
+  }],
+  deploy : {
+    production : {
+      user : 'videocms',
+      host : 'vcms.pepex.kg',
+      ref  : 'origin/master',
+      repo : 'git@github.com:PavelKg/Video-CMS-backend.git',
+      path : '~/services/videocms-backend',
+      'post-deploy' : 'npm i && pm2 startOrRestart ecosystem.config.js --env production'
+    },
+    development : {
+      user : 'videocms',
+      host : 'vcms.pepex.kg',
+      ref  : 'origin/development',
+      repo : 'git@github.com:PavelKg/Video-CMS-back.git',
+      path : '~/services/videocms-backend',
+      'post-deploy' : 'npm i &&  pm2 startOrRestart ecosystem.config.js --env development'
+    }
+  }
+}; 
