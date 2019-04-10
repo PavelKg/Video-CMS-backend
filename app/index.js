75fd67dc930274f491623e4991ddbf75bcd79eeb
pavel.pavlenko <pavel.pavlenko@it-atlantic.com>
2019-04-10 18:31:02 +0600

    fix cors

--- app/index.js
+++ app/index.js
@@ -64,7 +64,7 @@ module.exports = async function(fastify, opts) {
     .register(fp(connectToDatabase))
     .register(fp(decorateFastifyInstance))
     .register(cors, { 
-      method: '*',
+      origin: false,
       path: '*'
     })    
     // APIs modules
