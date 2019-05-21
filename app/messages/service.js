c01a874e9b4e9343f71e38936cdb608e17ca00e1
pavel.pavlenko <pavel.pavlenko@it-atlantic.com>
2019-05-21 15:35:39 +0600

    add new fiels to messages list

--- app/messages/service.js
+++ app/messages/service.js
@@ -24,13 +24,21 @@ class MessageService {
 
     const client = await this.db.connect()
     const {rows} = await client.query(
-      `SELECT message_id as mid, message_sender as sender,
-        message_receiver as receiver, message_subject as subject,
-        message_text as text, message_important as important,
-        messages.created_at, messages.deleted_at
-      FROM "messages", users
-      WHERE user_company_id=$1 and user_uid=$2 
-        and (message_receiver=user_id or message_sender=user_id) ${qFilter} 
+      `SELECT message_id as mid, 
+      send_users.user_uid as sender_uid, send_users.user_company_id as sender_cid ,send_comp.company_name as sender_cname,
+      rec_users.user_uid as receiver_uid, rec_users.user_company_id as receiver_cid ,rec_comp.company_name as receiver_cname,
+      message_subject as subject,
+            message_text as text, message_important as important,
+            messages.created_at, messages.deleted_at
+          FROM "messages", users as rec_users, companies as rec_comp, users as send_users, companies as send_comp
+          WHERE (
+            (send_users.user_company_id=$1 and send_users.user_uid=$2) 
+            or 
+            (rec_users.user_company_id=$1 and rec_users.user_uid=$2)
+            )
+            and send_comp.company_id=send_users.user_company_id 
+            and rec_comp.company_id=rec_users.user_company_id
+            and (message_receiver=rec_users.user_id and message_sender=send_users.user_id) ${qFilter} 
         ORDER BY ${qSort} LIMIT ${limit} OFFSET $3;`,
       [cid, uid, offset]
     )
