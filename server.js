30272911d964f49a4d6c00fb742bcaa7e46d6751
PavelKg <bprog.kg@gmail.com>
2021-04-03 12:37:27 +0600

    SERVER-TYPE was added

--- server.js
+++ server.js
@@ -2,13 +2,14 @@
 /* global require */
 // Read the .env file.
 require('dotenv').config()
-const fs =require('fs')
+const fs = require('fs')
 
 const https = {
   key: fs.readFileSync('ssl_keys/privkey.pem'),
   cert: fs.readFileSync('ssl_keys/fullchain.pem')
 }
-const SERVER_TYPE = 'https'
+
+const {SERVER_TYPE = 'http'} = process.env
 const add_opt = SERVER_TYPE === 'https' ? {https} : {}
 
 // Require the framework
@@ -16,7 +17,7 @@ const fastify = require('fastify')({
   logger: true,
   ignoreTrailingSlash: true,
   bodyLimit: 7291456,
-  ...add_opt,
+  ...add_opt
 })
 
 // Register swagger.
@@ -27,8 +28,6 @@ fastify.register(require('fastify-swagger'), swagger.options)
 // Register application as a normal plugin.
 const app = require('./app')
 fastify.register(app)
-
-
 
 // Start listening.
 const start = async () => {
